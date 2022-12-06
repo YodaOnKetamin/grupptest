bdaebc800ccbcf05e3e8f243d06941e7e68b3336
YodaOnKetamin <Simon.Ulander.06@gmail.com>
2022-12-06 10:51:14 +0100

    acend

--- Assets/Scripts/PontusTest.cs
+++ Assets/Scripts/PontusTest.cs
@@ -13,6 +13,6 @@ public class PontusTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(0, 1 , 0);
+        transform.position += new Vector3(0, 1, 0) * Time.deltaTime;
     }
 }
